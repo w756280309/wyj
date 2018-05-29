352982ca41153c8d9f3911ab68e561df3626d2b5
Angus <1498373496@qq.com>
2018-05-29 03:56:13 +0000

    尝试一下

---
+++ app/Http/Controllers/StaticPageController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StaticPageController extends Controller
+{
+    //
+    public function home(){
+        return view('home');
+    }
+
+    public function help(){
+        return view('help');
+    }
+
+    public function about(){
+        return view('about');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/','StaticPageController@home')->name('home');
+Route::get('help','StaticPageController@help')->name('help');
+Route::get('about','StaticPageController@about')->name('about');
